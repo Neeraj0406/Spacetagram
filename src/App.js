14c9cb959a73059e03ef85b6ce38a985252bb625
Neeraj0406 <neerajsingh0406@gmail.com>
2022-01-18 12:46:45 +0530

    minor update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React,{useState,useEffect} from 'react'
 import Navbar from './Component/Navbar'
 import Card from "./Component/Card"
 import InfiniteScroll from 'react-infinite-scroll-component'
-// require('dotenv').config()
-
 
 const App = () => {
 
   let [allData , setAllData] = useState([])
-    
 
   const loadData =async (count = 5)=>{
     const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=W1e9ao3FrIYvhg1SBII7XBhVbqHj3Ede8FtyTrPX&count=${count}`)
@@ -18,7 +15,6 @@ const App = () => {
    
   
   }
-  console.log(allData);
   
 
   useEffect(() => {
@@ -43,8 +39,7 @@ const App = () => {
             <Card {...data} key={id} />
           ))
         :
-        <p> </p>
-        
+        <p> </p>       
       }
       </InfiniteScroll>
      
