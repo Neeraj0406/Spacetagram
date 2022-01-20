e5fb200d881f6bd46c14a5185b1ebf18d793fdf1
Neeraj0406 <neerajsingh0406@gmail.com>
2022-01-20 15:43:11 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,46 @@
-import React,{useState,useEffect} from 'react'
-import Navbar from './Component/Navbar'
-import Card from "./Component/Card"
-import InfiniteScroll from 'react-infinite-scroll-component'
+import React, { useState, useEffect } from "react";
+import Navbar from "./Component/Navbar";
+import Card from "./Component/Card";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const App = () => {
+  let [allData, setAllData] = useState([]);
 
-  let [allData , setAllData] = useState([])
-
-  const loadData =async (count = 5)=>{
-    const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=W1e9ao3FrIYvhg1SBII7XBhVbqHj3Ede8FtyTrPX&count=${count}`)
+  const loadData = async (count = 5) => {
+    const res = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=W1e9ao3FrIYvhg1SBII7XBhVbqHj3Ede8FtyTrPX&count=${count}`
+    );
     const result = await res.json();
-    setAllData([...allData , ...result])
-    
-   
-  
-  }
-  
+    setAllData([...allData, ...result]);
+  };
 
   useEffect(() => {
-    loadData()
-  }, [])
+    loadData();
+  }, []);
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
 
-      <InfiniteScroll 
+      <InfiniteScroll
         dataLength={allData.length}
         next={loadData}
-        hasMore = {true}
-        loader={<h1 style={{textAlign:"center", marginTop : "5px"}}> Data is loading...</h1>}
+        hasMore={true}
+        loader={
+          <h1 style={{ textAlign: "center", marginTop: "5px" }}>
+            {" "}
+            Data is loading...
+          </h1>
+        }
       >
-      
-      {
-        allData.length>0 ? 
-            allData.map((data , id) =>(
-            
-            <Card {...data} key={id} />
-          ))
-        :
-        <p> </p>       
-      }
+        {allData.length > 0 ? (
+          allData.map((data, id) => <Card {...data} key={id} />)
+        ) : (
+          <p> </p>
+        )}
       </InfiniteScroll>
-     
-      
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
